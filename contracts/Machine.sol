795eed44e4b403ab6f0780f3e850d356c26e9612
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-09-13 23:32:18 -0300

    new: add values to calculator using method signature (not ABI)

--- contracts/Machine.sol
+++ contracts/Machine.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.9;
 import "./Storage.sol";
 
 contract Machine {
+    uint256 public calculateResult;
+    address public user;
     Storage store;
+
+    event calledByCall(uint256 first, uint256 second, bool success);
 
     constructor(Storage addr) {
         store = addr;
+        calculateResult = 0;
     }
 
     function saveValue(uint value) public returns (bool) {
@@ -17,5 +22,13 @@ contract Machine {
 
     function getValue() public view returns (uint) {
         return store.value();
+    }
+
+    function addWithCall(address calculator, uint256 first, uint256 second) public returns (uint256) {
+        (bool success, bytes memory result) = calculator.call(
+            abi.encodeWithSignature("add(uint256,uint256)", first, second)
+        );
+        emit calledByCall(first, second, success);
+        return abi.decode(result, (uint256));
     }
 }
--- test/Machine.ts
+++ test/Machine.ts
@@ -6,15 +6,23 @@ describe("Machine tests", async () => {
   async function deployMachineFixture(value: number = 0) {
     const [owner, user, sender, receiver, ...others] =
       await ethers.getSigners();
-    const accounts = { owner, user, sender, receiver, others };
+    const accounts = {
+      owner,
+      user,
+      sender,
+      receiver,
+      others,
+    };
 
     const Storage = await ethers.getContractFactory("Storage");
     const Machine = await ethers.getContractFactory("Machine");
+    const Calculator = await ethers.getContractFactory("Calculator");
 
     const storage = await Storage.deploy(value);
     const machine = await Machine.deploy(storage.address);
+    const calculator = await Calculator.deploy();
 
-    return { storage, machine, accounts };
+    return { storage, machine, calculator, accounts };
   }
 
   describe("Deployment", () => {
@@ -26,7 +34,7 @@ describe("Machine tests", async () => {
     });
   });
 
-  describe("save value", () => {
+  describe("#saveValue", () => {
     it("should update storage contract value", async () => {
       const newValue = 99;
 
@@ -36,6 +44,74 @@ describe("Machine tests", async () => {
       const result = await storage.value();
 
       expect(result).to.equal(newValue);
+    });
+  });
+
+  describe("#addWithCall", () => {
+    it("Should emit event on transfer with call", async () => {
+      const first = 1;
+      const second = 2;
+
+      const { machine, calculator } = await loadFixture(deployMachineFixture);
+
+      await expect(machine.addWithCall(calculator.address, first, second))
+        .to.emit(machine, "calledByCall")
+        .withArgs(first, second, true);
+    });
+
+    it("Should successfully call to calculator contract", async () => {
+      const first = 1;
+      const second = 2;
+
+      const { machine, calculator, accounts } = await loadFixture(
+        deployMachineFixture
+      );
+      const { owner } = accounts;
+
+      const { from, to } = await machine.addWithCall(
+        calculator.address,
+        first,
+        second
+      );
+
+      expect(from).equal(owner.address);
+      expect(to).equal(machine.address);
+    });
+
+    it("Should successfully add values", async () => {
+      const first = 1;
+      const second = 2;
+
+      const { machine, calculator } = await loadFixture(deployMachineFixture);
+
+      await machine.addWithCall(calculator.address, first, second);
+      const calculatedResult = await calculator.calculateResult();
+
+      expect(calculatedResult).equal(first + second);
+    });
+
+    it("Should not mutate caller contract state", async () => {
+      const { machine, calculator } = await loadFixture(deployMachineFixture);
+
+      await machine.addWithCall(calculator.address, 1, 2);
+
+      const calculatedResult = await calculator.calculateResult();
+      const machineCalculatedResult = await machine.calculateResult();
+
+      expect(calculatedResult).equal(3);
+      expect(machineCalculatedResult).equal(0);
+    });
+
+    it("Should validate user on each context", async () => {
+      const { machine, calculator } = await loadFixture(deployMachineFixture);
+
+      await machine.addWithCall(calculator.address, 1, 2);
+
+      const machineUser = await machine.user();
+      const calculatorUser = await calculator.user();
+
+      expect(machineUser).equal(ethers.constants.AddressZero);
+      expect(calculatorUser).equal(machine.address);
     });
   });
 });
